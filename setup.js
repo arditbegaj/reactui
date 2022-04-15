ed41d583a4a547c2319f481301162c00535999db
arditbegaj <90195845+arditbegaj@users.noreply.github.com>
2022-04-15 14:20:59 +0200

    Update setup.js

--- setup.js
+++ setup.js
@@ -4,6 +4,6 @@ const myArguments = process.argv.slice(2);
 const modules = JSON.parse(myArguments[0]).modules;
 console.log(modules)
 console.log(modules.identity.features[0])
-fse.writeFile(`${__dirname}/src/pages/${modules.identity.features[0].tsx}`, 'Success', 'utf8');
+fse.writeFile(`${__dirname}/src/pages/${modules.identity.features[0]}.tsx`, 'Success', 'utf8');
 
 
