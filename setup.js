ff25f6463a1cd51770ab0b7480f7a3f938c1f1f3
arditbegaj <90195845+arditbegaj@users.noreply.github.com>
2022-04-15 11:20:04 +0200

    Update setup.js

--- setup.js
+++ setup.js
@@ -1,4 +1,4 @@
+console.log(process.argv)
 const myArguments = process.argv.slice(2);
-//const obj = JSON.parse(myArguments[0])
-console.log(myArguments);
-
+const obj = JSON.stringify(myArguments[0])
+console.log(obj, 'obj');
