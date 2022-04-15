cba9ecfd0d2f590c53828901a34c71d317c138a1
arditbegaj <90195845+arditbegaj@users.noreply.github.com>
2022-04-15 10:42:41 +0200

    Update setup.js

--- setup.js
+++ setup.js
@@ -1,6 +1,4 @@
 const myArguments = process.argv.slice(2);
-const obj = JSON.parse(myArguments[0]);
-const modules = obj.modules;
-console.log(modules)
-// console.log(myArguments[0]);
+const obj = JSON.parse(myArguments[0])
+console.log(obj, 'obj');
 
