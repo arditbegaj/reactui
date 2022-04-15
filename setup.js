dc434f69c352402400ee4dd41c3fe21dfeb7a7c2
arditbegaj <90195845+arditbegaj@users.noreply.github.com>
2022-04-15 13:49:46 +0200

    Update setup.js

--- setup.js
+++ setup.js
@@ -2,11 +2,7 @@ import * as fs from 'fs-extra';
 
 const myArguments = process.argv.slice(2);
 const modules = JSON.parse(myArguments[0]).modules;
+console.log(modules)
 console.log(modules.identity.features[0])
 
 await fs.writeFile(`src/pages/${modules.identity.features[0]}`, 'Success', 'utf8');
-
-
-
-
-
